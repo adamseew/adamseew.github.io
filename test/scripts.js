ee8e3ee3952b79ed09d3d3f70214811d5a1a7ca6
Adam Seewald <adam.seewald@studenti.univr.it>
2019-09-22 22:04:49 +0200

    just a test of the new webpage

---
+++ test/scripts.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+    function adjust_menu(x) {
+        if (x.matches) {
+            document.getElementById('menu_div2').classList.remove('pure-menu-horizontal');
+            document.getElementById('menu_div1').classList.remove('topmenu1');
+            document.getElementsByTagName('body')[0].style.paddingTop = 0;
+            document.getElementById('menu_div2').classList.add('custom-restricted-width');
+        } else {
+            document.getElementById('menu_div2').classList.add('pure-menu-horizontal');
+            document.getElementById('menu_div1').classList.add('topmenu1');
+            document.getElementsByTagName('body')[0].style.paddingTop = '48px';
+            document.getElementById('menu_div2').classList.remove('custom-restricted-width');
+        }
+    }
+
+    var x = window.matchMedia('(min-width: 48em)')
+    adjust_menu(x)
+    x.addListener(adjust_menu)
+}
