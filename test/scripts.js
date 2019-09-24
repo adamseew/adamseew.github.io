496ee8a13656b8908cb1e93e5ff861c4f1c3b1c2
Adam Seewald <adam.seewald@studenti.univr.it>
2019-09-25 00:32:10 +0200

    updates to the new webpage

--- test/scripts.js
+++ test/scripts.js
@@ -2,15 +2,15 @@
 window.onload = function () {
     function adjust_menu(x) {
         if (x.matches) {
-            document.getElementById('menu_div2').classList.remove('pure-menu-horizontal');
-            document.getElementById('menu_div1').classList.remove('topmenu1');
+            document.getElementById('menu-div2').classList.remove('pure-menu-horizontal');
+            document.getElementById('menu-div1').classList.remove('top-menu');
             document.getElementsByTagName('body')[0].style.paddingTop = 0;
-            document.getElementById('menu_div2').classList.add('custom-restricted-width');
+            document.getElementById('menu-div2').classList.add('custom-restricted-width');
         } else {
-            document.getElementById('menu_div2').classList.add('pure-menu-horizontal');
-            document.getElementById('menu_div1').classList.add('topmenu1');
+            document.getElementById('menu-div2').classList.add('pure-menu-horizontal');
+            document.getElementById('menu-div1').classList.add('top-menu');
             document.getElementsByTagName('body')[0].style.paddingTop = '48px';
-            document.getElementById('menu_div2').classList.remove('custom-restricted-width');
+            document.getElementById('menu-div2').classList.remove('custom-restricted-width');
         }
     }
 
